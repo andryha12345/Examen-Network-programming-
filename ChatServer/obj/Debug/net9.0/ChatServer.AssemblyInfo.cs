3e9970371d6a21adb028ef4b5221908c94d54454
Zhide_gk90@student.itstep.org <Zhide_gk90@student.itstep.org>
2026-06-12 00:03:40 +0300

    Test 2 .2

--- ChatServer/obj/Debug/net9.0/ChatServer.AssemblyInfo.cs
+++ ChatServer/obj/Debug/net9.0/ChatServer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChatServer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+567768b407cf1b54bdfd0263a5afb7a70be09dab")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3c928ed19f60e217c88a405a27d5c4cb4cd98556")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChatServer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChatServer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
